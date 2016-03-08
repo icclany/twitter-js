0d9c960e69ae175986f1e46a2232eeb9aae52055
seanbert4 <seanbert4@gmail.com>
2016-03-08 13:14:35 -0500

    Where we stopped before lunch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
+var swig = require('swig') //creates instance of swig
 
 // Log message upon connection
 app.get("/", function(request, response) {
