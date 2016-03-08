5c26263cbbef9cb4e7d2e8b36438e546325ab684
Iris <icc.lany@gmail.com>
2016-03-08 15:18:18 -0500

    Added swig functionality

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
-var swig = require('swig') //creates instance of swig
+var swig = require('swig'); //creates instance of swig
+swig.setDefaults({cache: false}); // turn off swig's caching
 
 // Log message upon connection
 app.get("/", function(request, response) {
-	response.send("Server listening.");
+	response.render( 'index', {title: 'Hall of Fame', people: people} );
 })
-
 
 // Listen for requests
 app.listen(3000, function() {
 	console.log("Server listening.");
 });
 
+// Uses swig.renderFile as the function to render html
+app.engine('html', swig.renderFile);
 
-//Extra comment
+// Set the default view engine to html
+app.set('view engine', 'html');
+
+// Set the views path to our views folder
+app.set('views', __dirname + '/views');
+
+// Render index views example
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
